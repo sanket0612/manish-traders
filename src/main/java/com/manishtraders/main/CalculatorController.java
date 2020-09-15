b3e101806eaf56843b9abc1cc71ba9fc540815ed
Sanket Barapatre <sanket.barapatre@thoughtworks.com>
2020-09-15 15:03:27 +0530

    Initial Commit

---
+++ src/main/java/com/manishtraders/main/CalculatorController.java
@@ -0,0 +1,14 @@
+package com.manishtraders.main;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello, Chanki";
+    }
+}
---
+++ src/main/java/com/manishtraders/main/ChankiApplication.java
@@ -0,0 +1,13 @@
+package com.manishtraders.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+ChankiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChankiApplication.class);
+    }
+}
