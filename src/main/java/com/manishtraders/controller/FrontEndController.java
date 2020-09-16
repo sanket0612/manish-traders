f1f45fd8fb01825d994af16d347e4e4863b8a430
Sanket Barapatre <sanket.barapatre@thoughtworks.com>
2020-09-16 22:28:53 +0530

    Sanket | Enhancement | Added Swagger

---
+++ src/main/java/com/manishtraders/controller/FrontEndController.java
@@ -0,0 +1,24 @@
+package com.manishtraders.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FrontEndController {
+
+    @GetMapping("/greeting")
+    public String index(Model model, @RequestParam(defaultValue = "Customer") String name){
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello, Welcome";
+    }
+
+}
